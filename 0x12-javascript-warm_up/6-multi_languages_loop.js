5ebb8ce87ace35d1d98bf950705df0f48c50f23b
jmsnyaga <jmsnyagak@gmail.com>
2024-09-02 12:40:28 +0300

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const l in lang) {
-  console.log(lang[l]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
